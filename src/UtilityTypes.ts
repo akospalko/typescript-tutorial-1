08b2839518402c17dfd8a59dc302f929adba02e3
Agathius <p.akos1148@gmail.com>
2023-08-19 23:53:05 +0200

    created file utility type

---
+++ src/UtilityTypes.ts
@@ -0,0 +1,9 @@
+//----------Utility types----------
+export const UtilityTypes = ():void => {
+  /**
+  useful for common type transformations - allows us to pass only a part of an obj/interface  
+   
+  
+  */ 
+
+}
